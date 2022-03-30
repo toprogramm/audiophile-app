d10cf7d2b8e888b3b1e1562941883c32fa2166bc
Olia Dovbush <oliaivanchushun@gmail.com>
2022-03-30 14:47:53 +0300

    add design system

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import './scss/App.scss';
 
 function App() {
-  return;
+  return (
+    <>
+      <button className="button_orange">See product</button>
+      <button className="button_white">See product</button>
+      <button className="button_arrow">Shop</button>
+    </>
+  );
 }
 
 export default App;
