8af158e9b50149c7868f4dbf4f19bbdb84fa357e
Olia Dovbush <oliaivanchushun@gmail.com>
2022-03-30 17:37:12 +0300

    add form elements design

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,52 @@ function App() {
       <button className="button_orange">See product</button>
       <button className="button_white">See product</button>
       <button className="button_arrow">Shop</button>
+
+      <div>
+        <input
+          className="input_text"
+          type="text"
+          placeholder="enter your name"
+        />
+      </div>
+
+      <div className="payment">
+        <div className="payment__title">Payment Method:</div>
+        <div className="payment__box">
+          <label className="payment__label">
+            <span className="payment__text">e-Money</span>
+            <input
+              className="payment__input"
+              type="radio"
+              name="payment"
+              placeholder="e-Money"
+            />
+            <span className="payment__radio">
+              <span></span>
+            </span>
+          </label>
+          <label className="payment__label">
+            <span className="payment__text">Cash on Delivery</span>
+            <input
+              className="payment__input"
+              type="radio"
+              name="payment"
+              checked
+            />
+            <span className="payment__radio">
+              <span></span>
+            </span>
+          </label>
+        </div>
+      </div>
+
+      <div className="number">
+        <div className="number__field">
+          <input className="product" type="number" value="1" min="1" />
+        </div>
+        <div className="number__spin minus"></div>
+        <div className="number__spin plus"></div>
+      </div>
     </>
   );
 }
